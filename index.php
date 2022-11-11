6a155e54e9ce8614a009bc788060eec26ee559c5
Paulo Peixoto <90398620+Spanyk@users.noreply.github.com>
2022-11-11 13:14:00 -0300

    alteração nos campos

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+if(!isset())
 include_once 'nav/header.php';
 include_once 'nav/login.php';
 include_once 'nav/footer.php';
--- nav/login.php
+++ nav/login.php
@@ -1,21 +1,21 @@
 <div class="container" id="container">
     <div class="form-container sign-up-container">
-        <form action="#">
+        <form method="post">
             <h1>Crie uma conta</h1>
             <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#" class="social"><img src="assets/img/pipoca.png"></a>
+                <a href="#" class="social"><img src="assets/img/cinema.png"></a>
+                <a href="#" class="social"><img src="assets/img/tira-de-filme.png"></a>
             </div>
             <span>Ou use seu email para se registrar</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" id="username"/>
+            <input type="email" placeholder="Email" id="email"/>
+            <input type="password" placeholder="Password" id="user_password" />
             <button>Entrar</button>
         </form>
     </div>
     <div class="form-container sign-in-container">
-        <form action="#">
+        <form method="post">
             <h1>Login</h1>
             <div class="social-container">
                 <a href="#" class="social"><img src="assets/img/pipoca.png"></a>
@@ -23,8 +23,8 @@
                 <a href="#" class="social"><img src="assets/img/tira-de-filme.png"></a>
             </div>
             <span>Ou use sua conta</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input type="email" placeholder="Email" id="email" />
+            <input type="password" placeholder="Password" id="user_password"/>
             <a href="#" id="alt_senha">Esqueceu sua senha?</a>
             <button>Entrar</button>
         </form>
