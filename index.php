2cec25bc69bd1f766231179f301aec04e1fa5fc5
paulo.augusto62@hotmail.com <paulo.augusto62@hotmail.com>
2022-11-22 19:08:41 -0300

    [alt] temporaria

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
-if(!isset())
-include_once 'nav/header.php';
-include_once 'nav/login.php';
-include_once 'nav/footer.php';
+include_once 'header.php';
+// include_once 'aut/login.php';
+// include_once 'nav/categorias.php';
+include_once 'footer.php'; 
+
 ?>
+    <div class="">
+    <header>
+        <div>
+            <img src="/assets/img/milenium-banner.png" alt="">
+        </div>
+    </header>
+</div>
+
+<?php include_once 'footer.php'; ?>
